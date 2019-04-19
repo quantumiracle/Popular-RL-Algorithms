07f85b08614f632287e87cdc63e5f056c1f725c9
quantumiracle <1402434478@qq.com>
2019-04-19 20:57:39 +0100

    sac+reacher

---
+++ reacher.py
@@ -0,0 +1,178 @@
+import pygame
+import numpy as np
+import math
+import time
+from gym.spaces.box import Box
+import matplotlib.pyplot as plt
+
+class Reacher:
+    def __init__(self, screen_size=1000, num_joints=2, link_lengths = [200, 140], ini_joint_angles=[0.1, 0.1], target_pos = [669,430], render=False):
+        # Global variables
+        self.screen_size = screen_size
+        self.num_joints = num_joints # not counting the initial one
+        self.link_lengths = link_lengths
+        self.ini_joint_angles = ini_joint_angles
+        self.joint_angles = ini_joint_angles
+        self.num_actions = self.num_joints
+        self.num_observations= 2*(self.num_actions+2) # initial joint +1, target position +1
+        self.L = 8 # distance from target to get reward 2
+        self.action_space=Box(-100,100, [self.num_actions])
+        self.observation_space=Box(-1000,1000, [2*(self.num_actions+2)])
+        self.target_pos=target_pos
+
+        self.render=render
+        if self.render == True:
+            self.screen = pygame.display.set_mode((self.screen_size, self.screen_size))
+            pygame.display.set_caption("Reacher")
+        else:
+            pass
+
+        self.is_running = 1
+        self.steps=0
+        self.max_episode_steps=50
+        self.near_goal_range=0.5
+        self.change_goal=0
+        self.change_goal_episodes=10
+
+    # Function to compute the transformation matrix between two frames
+    def compute_trans_mat(self, angle, length):
+        cos_theta = math.cos(math.radians(angle))
+        sin_theta = math.sin(math.radians(angle))
+        dx = -length * sin_theta
+        dy = length * cos_theta
+        T = np.array([[cos_theta, -sin_theta, dx], [sin_theta, cos_theta, dy], [0, 0, 1]])
+        return T
+
+    # Function to draw the current state of the world
+    def draw_current_state(self, ):
+        T = np.zeros((self.num_joints, 3, 3))  # transition matrix
+        origin = np.zeros((self.num_joints, 3)) # transformed coordinates - 3 values
+        p = np.zeros((self.num_joints+1, 2))  # joint coordinates in world
+        p[0] = [0, 0] # initial joint coordinates
+
+        for i in range(self.num_joints):
+            T[i] = self.compute_trans_mat(self.joint_angles[i], self.link_lengths[i])
+            multiplier = np.array([0, 0, 1])
+            for j in range(i):
+                multiplier=np.dot(T[i-j], multiplier)
+            origin[i] = np.dot(T[0], multiplier)
+            p[i+1] = [origin[i][0], -1.*origin[i][1]]  # the - is because the y-axis is opposite in world and image coordinates
+        
+        int_coordinates = [[0 for i in range(2)] for j in range(self.num_joints+1)]
+        for i in range (self.num_joints+1):
+            int_coordinates[i][0] = int(0.5 * self.screen_size + p[i][0])
+            int_coordinates[i][1] = int(0.5 * self.screen_size + p[i][1])
+
+
+        if self.render == True:
+            self.screen.fill((0, 0, 0))
+            for i in range (self.num_joints+1):
+                if i < self.num_joints:
+                    pygame.draw.line(self.screen, (255, 255, 255), [int_coordinates[i][0], int_coordinates[i][1]], [int_coordinates[i+1][0], int_coordinates[i+1][1]], 5) # draw link
+                pygame.draw.circle(self.screen, (0, 255, 0), [int_coordinates[i][0], int_coordinates[i][1]], 10)  # draw joint
+            pygame.draw.circle(self.screen, (255, 255, 0), np.array(self.target_pos).astype(int), 10) # draw target
+            # Flip the display buffers to show the current rendering
+            pygame.display.flip()
+            time.sleep(0.5)
+
+            ''' screenshot the image '''
+            # pygame.image.save(self.screen, './screen.png')
+            array_screen = pygame.surfarray.array3d(self.screen) # 3d array pygame.surface (self.screen)
+            red_array_screen=pygame.surfarray.pixels_red(self.screen) # 2d array from red pixel of pygame.surface (self.screen)
+            downsampling_rate=5 # downsmaple the screen shot, origin 1000*1000*3
+            downsampled_array_screen=array_screen[::downsampling_rate,::downsampling_rate,]
+            # plt.imshow(array_screen[::downsampling_rate,::downsampling_rate,])
+            # plt.show()
+            
+        else:
+            pass
+        return p.reshape(-1), np.array([downsampled_array_screen])
+    
+    def reset(self, screen_shot):
+        ''' reset the environment '''
+        self.steps=0
+        self.joint_angles = np.array(self.ini_joint_angles)*180.0/np.pi
+        if self.render == True:
+            self.screen = pygame.display.set_mode((self.screen_size, self.screen_size))
+            pygame.display.set_caption("Reacher")
+        else:
+            pass
+        self.is_running = 1
+
+        ''' reset the target position for learning across tasks '''
+        # self.change_goal+=1
+        # if self.change_goal > self.change_goal_episodes:
+        #     self.change_goal=0
+        #     range_pose=0.3
+        #     target_pos=range_pose*np.random.rand(2) + [0.5,0.5]
+        #     self.target_pos=target_pos*self.screen_size
+
+        pos_set, screenshot=self.draw_current_state()
+        
+        if screen_shot:
+            return screenshot
+        else:
+            return np.array(np.concatenate((pos_set,self.target_pos)))
+
+    def step(self,action, sparse_reward, screen_shot):    
+        # Get events and check if the user has closed the window
+        if self.render == True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.is_running = 0
+                    break
+        else:
+            pass
+        # Change the joint angles (the increment is in degrees)
+        for i in range (self.num_joints):
+            self.joint_angles[i] += action[i]
+
+        pos_set, screenshot=self.draw_current_state()
+        distance2goal = np.sqrt((pos_set[-2]-self.target_pos[0])**2+(pos_set[-1]-self.target_pos[1])**2)
+        if sparse_reward:
+            if distance2goal < self.L:
+                reward = 20
+            else:
+                reward = -1
+        
+        else:    
+            reward_0=100.0
+            reward = reward_0 / (np.sqrt((pos_set[-2]-self.target_pos[0])**2+(pos_set[-1]-self.target_pos[1])**2)+1)
+        if screen_shot: 
+            return screenshot, np.array(reward), np.array(0), distance2goal
+        else: 
+            return np.array(np.concatenate((pos_set,self.target_pos))), np.array(reward), np.array(0), distance2goal
+
+
+if __name__ == "__main__":
+
+
+    num_episodes=500
+    num_steps=20
+    action_range=5.0
+    NUM_JOINTS=4
+    LINK_LENGTH=[200, 140, 80, 50]
+    INI_JOING_ANGLES=[0.1, 0.1, 0.1, 0.1]
+    SPARSE_REWARD=False
+    SCREEN_SHOT=False
+
+    # reacher=Reacher(render=True)  # 2-joint reacher
+    reacher=Reacher(screen_size=1000, num_joints=NUM_JOINTS, link_lengths = LINK_LENGTH, \
+    ini_joint_angles=INI_JOING_ANGLES, target_pos = [669,430], render=True)
+
+
+    epi=0
+    while epi<num_episodes:
+        print(epi)
+        epi+=1
+        step=0
+        reacher.reset(SCREEN_SHOT)
+        while step<num_steps:
+            step+=1
+            action=np.random.uniform(-action_range,action_range,size=NUM_JOINTS)
+            re=reacher.step([action], SPARSE_REWARD, SCREEN_SHOT)
+            print(re)
+
+
+
+    
---
+++ sac.py
@@ -0,0 +1,316 @@
+'''
+Soft Actor-Critic
+'''
+
+
+import math
+import random
+
+import gym
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.distributions import Normal
+
+from IPython.display import clear_output
+import matplotlib.pyplot as plt
+from matplotlib import animation
+from IPython.display import display
+from reacher import Reacher
+
+
+use_cuda = torch.cuda.is_available()
+device   = torch.device("cuda" if use_cuda else "cpu")
+print(device)
+
+
+class ReplayBuffer:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.buffer = []
+        self.position = 0
+    
+    def push(self, state, action, reward, next_state, done):
+        if len(self.buffer) < self.capacity:
+            self.buffer.append(None)
+        self.buffer[self.position] = (state, action, reward, next_state, done)
+        self.position = int((self.position + 1) % self.capacity)  # as a ring buffer
+    
+    def sample(self, batch_size):
+        batch = random.sample(self.buffer, batch_size)
+        state, action, reward, next_state, done = map(np.stack, zip(*batch)) # stack for each element
+        ''' 
+        the * serves as unpack: sum(a,b) <=> batch=(a,b), sum(*batch) ;
+        zip: a=[1,2], b=[2,3], zip(a,b) => [(1, 2), (2, 3)] ;
+        the map serves as mapping the function on each list element: map(square, [2,3]) => [4,9] ;
+        np.stack((1,2)) => array([1, 2])
+        '''
+        return state, action, reward, next_state, done
+    
+    def __len__(self):
+        return len(self.buffer)
+
+# class NormalizedActions(gym.ActionWrapper):
+#     def _action(self, action):
+#         low  = self.action_space.low
+#         high = self.action_space.high
+        
+#         action = low + (action + 1.0) * 0.5 * (high - low)
+#         action = np.clip(action, low, high)
+        
+#         return action
+
+#     def _reverse_action(self, action):
+#         low  = self.action_space.low
+#         high = self.action_space.high
+        
+#         action = 2 * (action - low) / (high - low) - 1
+#         action = np.clip(action, low, high)
+        
+#         return action
+
+def plot(frame_idx, rewards):
+    clear_output(True)
+    plt.figure(figsize=(20,5))
+    plt.subplot(131)
+    plt.title('frame %s. reward: %s' % (frame_idx, rewards[-1]))
+    plt.plot(rewards)
+    plt.show()
+
+class ValueNetwork(nn.Module):
+    def __init__(self, state_dim, hidden_dim, init_w=3e-3):
+        super(ValueNetwork, self).__init__()
+        
+        self.linear1 = nn.Linear(state_dim, hidden_dim)
+        self.linear2 = nn.Linear(hidden_dim, hidden_dim)
+        self.linear3 = nn.Linear(hidden_dim, 1)
+        # weights initialization
+        self.linear3.weight.data.uniform_(-init_w, init_w)
+        self.linear3.bias.data.uniform_(-init_w, init_w)
+        
+    def forward(self, state):
+        x = F.relu(self.linear1(state))
+        x = F.relu(self.linear2(x))
+        x = self.linear3(x)
+        return x
+        
+        
+class SoftQNetwork(nn.Module):
+    def __init__(self, num_inputs, num_actions, hidden_size, init_w=3e-3):
+        super(SoftQNetwork, self).__init__()
+        
+        self.linear1 = nn.Linear(num_inputs + num_actions, hidden_size)
+        self.linear2 = nn.Linear(hidden_size, hidden_size)
+        self.linear3 = nn.Linear(hidden_size, 1)
+        
+        self.linear3.weight.data.uniform_(-init_w, init_w)
+        self.linear3.bias.data.uniform_(-init_w, init_w)
+        
+    def forward(self, state, action):
+        x = torch.cat([state, action], 1) # the dim 0 is number of samples
+        x = F.relu(self.linear1(x))
+        x = F.relu(self.linear2(x))
+        x = self.linear3(x)
+        return x
+        
+        
+class PolicyNetwork(nn.Module):
+    def __init__(self, num_inputs, num_actions, hidden_size, init_w=3e-3, log_std_min=-20, log_std_max=2):
+        super(PolicyNetwork, self).__init__()
+        
+        self.log_std_min = log_std_min
+        self.log_std_max = log_std_max
+        
+        self.linear1 = nn.Linear(num_inputs, hidden_size)
+        self.linear2 = nn.Linear(hidden_size, hidden_size)
+        
+        self.mean_linear = nn.Linear(hidden_size, num_actions)
+        self.mean_linear.weight.data.uniform_(-init_w, init_w)
+        self.mean_linear.bias.data.uniform_(-init_w, init_w)
+        
+        self.log_std_linear = nn.Linear(hidden_size, num_actions)
+        self.log_std_linear.weight.data.uniform_(-init_w, init_w)
+        self.log_std_linear.bias.data.uniform_(-init_w, init_w)
+        
+    def forward(self, state):
+        x = F.relu(self.linear1(state))
+        x = F.relu(self.linear2(x))
+        
+        mean    = self.mean_linear(x)
+        log_std = self.log_std_linear(x)
+        log_std = torch.clamp(log_std, self.log_std_min, self.log_std_max)
+        
+        return mean, log_std
+    
+    def evaluate(self, state, epsilon=1e-6):
+        '''
+        generate sampled action with state as input wrt the policy network;
+        '''
+        mean, log_std = self.forward(state)
+        std = log_std.exp()
+        
+        normal = Normal(0, 1)
+        z      = normal.sample()
+        action = torch.tanh(mean+ std*z.to(device)) # TanhNormal distribution as actions; reparameterization trick
+        log_prob = Normal(mean, std).log_prob(mean+ std*z.to(device)) - torch.log(1 - action.pow(2) + epsilon) #?; dim is action's dim?
+        log_prob = log_prob.sum(dim=1, keepdim=True)
+        return action, log_prob, z, mean, log_std
+        
+    
+    def get_action(self, state):
+        state = torch.FloatTensor(state).unsqueeze(0).to(device)
+        mean, log_std = self.forward(state)
+        std = log_std.exp()
+        
+        normal = Normal(0, 1)
+        z      = normal.sample().to(device)
+        action = torch.tanh(mean + std*z)
+        
+        action  = action.cpu()#.detach().cpu().numpy()
+        return action[0]
+
+
+def update(batch_size,gamma=0.99,soft_tau=1e-2,):
+    
+    state, action, reward, next_state, done = replay_buffer.sample(batch_size)
+    # print('sample:', state, action,  reward, done)
+
+    state      = torch.FloatTensor(state).to(device)
+    next_state = torch.FloatTensor(next_state).to(device)
+    action     = torch.FloatTensor(action).to(device)
+    reward     = torch.FloatTensor(reward).unsqueeze(1).to(device)  # reward is single value, unsqueeze() to add one dim to be [reward] at the sample dim;
+    done       = torch.FloatTensor(np.float32(done)).unsqueeze(1).to(device)
+
+    predicted_q_value1 = soft_q_net1(state, action)
+    predicted_q_value2 = soft_q_net2(state, action)
+    predicted_value    = value_net(state)
+    new_action, log_prob, epsilon, mean, log_std = policy_net.evaluate(state)
+
+# Training Q Function
+    target_value = target_value_net(next_state)
+    target_q_value = reward + (1 - done) * gamma * target_value # if done==1, only reward
+    q_value_loss1 = soft_q_criterion1(predicted_q_value1, target_q_value.detach())  # detach: no gradients for the variable
+    q_value_loss2 = soft_q_criterion2(predicted_q_value2, target_q_value.detach())
+
+
+    soft_q_optimizer1.zero_grad()
+    q_value_loss1.backward()
+    soft_q_optimizer1.step()
+    soft_q_optimizer2.zero_grad()
+    q_value_loss2.backward()
+    soft_q_optimizer2.step()  
+
+# Training Value Function
+    predicted_new_q_value = torch.min(soft_q_net1(state, new_action),soft_q_net2(state, new_action))
+    target_value_func = predicted_new_q_value - log_prob
+    # print('pre v: ', predicted_value)
+    # print('pre n q: ', predicted_new_q_value)
+    # print('log p: ', log_prob)
+    # print('t v: ', target_value_func)
+    value_loss = value_criterion(predicted_value, target_value_func.detach())
+
+    
+    value_optimizer.zero_grad()
+    value_loss.backward()
+    value_optimizer.step()
+
+# Training Policy Function
+    policy_loss = (log_prob - predicted_new_q_value).mean()
+
+    policy_optimizer.zero_grad()
+    policy_loss.backward()
+    policy_optimizer.step()
+    
+    
+    for target_param, param in zip(target_value_net.parameters(), value_net.parameters()):
+        target_param.data.copy_(  # copy data value into target parameters
+            target_param.data * (1.0 - soft_tau) + param.data * soft_tau
+        )
+
+
+# intialization
+# NUM_JOINTS=4
+# LINK_LENGTH=[200, 140, 80, 50]
+# INI_JOING_ANGLES=[0.1, 0.1, 0.1, 0.1]
+NUM_JOINTS=2
+LINK_LENGTH=[200, 140]
+INI_JOING_ANGLES=[0.1, 0.1]
+SPARSE_REWARD=False
+SCREEN_SHOT=False
+env=Reacher(screen_size=1000, num_joints=NUM_JOINTS, link_lengths = LINK_LENGTH, \
+ini_joint_angles=INI_JOING_ANGLES, target_pos = [669,430], render=True)
+action_dim = env.num_actions
+state_dim  = env.num_observations
+hidden_dim = 256
+
+value_net        = ValueNetwork(state_dim, hidden_dim).to(device)
+target_value_net = ValueNetwork(state_dim, hidden_dim).to(device)
+
+soft_q_net1 = SoftQNetwork(state_dim, action_dim, hidden_dim).to(device)
+soft_q_net2 = SoftQNetwork(state_dim, action_dim, hidden_dim).to(device)
+policy_net = PolicyNetwork(state_dim, action_dim, hidden_dim).to(device)
+
+print('(Target) Value Network: ', value_net)
+print('Soft Q Network (1,2): ', soft_q_net1)
+print('Policy Network: ', policy_net)
+
+for target_param, param in zip(target_value_net.parameters(), value_net.parameters()):
+    target_param.data.copy_(param.data)
+    
+
+value_criterion  = nn.MSELoss()
+soft_q_criterion1 = nn.MSELoss()
+soft_q_criterion2 = nn.MSELoss()
+
+value_lr  = 3e-4
+soft_q_lr = 3e-4
+policy_lr = 3e-4
+
+value_optimizer  = optim.Adam(value_net.parameters(), lr=value_lr)
+soft_q_optimizer1 = optim.Adam(soft_q_net1.parameters(), lr=soft_q_lr)
+soft_q_optimizer2 = optim.Adam(soft_q_net2.parameters(), lr=soft_q_lr)
+policy_optimizer = optim.Adam(policy_net.parameters(), lr=policy_lr)
+
+
+replay_buffer_size = 1e6
+replay_buffer = ReplayBuffer(replay_buffer_size)
+
+
+# hyper-parameters
+max_frames  = 40000
+max_steps   = 20
+frame_idx   = 0
+rewards     = []
+batch_size  = 2
+
+
+# training loop
+while frame_idx < max_frames:
+    state = env.reset(SCREEN_SHOT)
+    episode_reward = 0
+    
+    
+    for step in range(max_steps):
+        action = policy_net.get_action(state).detach()
+        next_state, reward, done, _ = env.step(action.numpy(), SPARSE_REWARD, SCREEN_SHOT)
+        replay_buffer.push(state, action, reward, next_state, done)
+        
+        state = next_state
+        episode_reward += reward
+        frame_idx += 1
+        print(frame_idx)
+        
+        if len(replay_buffer) > batch_size:
+            update(batch_size)
+        
+        if frame_idx % 100 == 0:
+            plot(frame_idx, rewards)
+        
+        if done:
+            break
+        
+    rewards.append(episode_reward)
+
